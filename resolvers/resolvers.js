ba704557c390678b70bcbe48d584066d2401ac9e
Zulkarnine1 <iamzulkarnine@gmail.com>
2021-08-18 22:05:32 +0600

    A few comments added

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -25,16 +25,22 @@ const resolvers = {
       return entry;
     },
     updateCountryEntryById: async (_, args, context, info) => {
+      // Deconstructing the input country
       const { name, year, area, total_population } = args.country;
+      // Getting the country entry doc to be updated
       const entry = await CountryEntry.findById(args.id);
+      // Checking which attributes to be updated
       if (name !== undefined) entry.name = name;
       if (year !== undefined) entry.year = year;
       if (area !== undefined) entry.area = area;
       if (total_population !== undefined) entry.total_population = total_population;
+
+      // saving the updates and returning the entry
       await entry.save();
       return entry;
     },
     deleteCountryEntryByID: async (_, args, context, info) => {
+      // Getting and removing the entry
       const entry = await CountryEntry.findById(args.id);
       entry.remove();
       return entry;
